24da9d404b1cad56dd4f00b2d175691dd6799601
s00154367 <s00154367@mail.itsligo.ie>
2017-03-08 18:45:35 +0000

    Choose Poster Added

--- WebCreateQR/Controllers/EventsController.cs
+++ WebCreateQR/Controllers/EventsController.cs
@@ -111,7 +111,7 @@ namespace WebCreateQR.Controllers
             return View(@event);
         }
 
-        // POST: Events/Edit/5
+        // POST: Events/EditPoster/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
